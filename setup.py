264ff84841728cb367cf3891a0714732e7d05f92
Will Crichton <wcrichto@cs.stanford.edu>
2019-07-04 14:30:13 -0700

    Add initial project

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+import os
+
+if __name__ == "__main__":
+    setup(name='autoplan',
+          version='0.1.0',
+          description='A system for program analysis',
+          url='http://github.com/willcrichton/autoplan',
+          author='Will Crichton and Georgia Sampaio',
+          author_email='wcrichto@cs.stanford.edu',
+          license='Apache 2.0',
+          install_requires=['torch >= 1.0.0'],
+          packages=['autoplan'],
+          zip_safe=False)
