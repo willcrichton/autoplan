27ecfd64227612d04e5b246519bb9fc5c1c5f861
Will Crichton <wcrichto@cs.stanford.edu>
2019-07-21 14:46:39 -0700

    Add Pyret tokenizer

--- autoplan/token.py
+++ autoplan/token.py
@@ -3,6 +3,9 @@ import subprocess as sp
 import os
 import json
 
+class TokenizerError(Exception):
+    pass
+
 class Tokenizer:
     def tokenize(self, program_string):
         raise NotImplementedError
@@ -23,6 +26,17 @@ class Tokenizer:
 
         return tokens, token_to_index, token_indices
 
+    def _call_tokenizer_process(self, program_string, rel_path):
+        file_dir = os.path.dirname(os.path.abspath(__file__))
+        binary = f'{file_dir}/tokenizers/{rel_path}'
+        p = sp.Popen([binary], stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE,
+                     cwd=os.path.dirname(binary))
+        stdout, stderr = p.communicate(input=program_string.encode())
+        try:
+            return json.loads(stdout.decode('utf-8'))
+        except json.JSONDecodeError:
+            raise TokenizerError(stdout, stderr)
+
 
 class JavaTokenizer(Tokenizer):
     def tokenize(self, program_string):
@@ -39,9 +53,16 @@ class JavaTokenizer(Tokenizer):
 
 class OCamlTokenizer(Tokenizer):
     def tokenize(self, program_string):
-        file_dir = os.path.dirname(os.path.abspath(__file__))
-        binary = f'{file_dir}/tokenizers/ocaml/main.native'
-        p = sp.Popen([binary], stdin=sp.PIPE, stdout=sp.PIPE)
-        tokens = json.loads(p.communicate(input=program_string.encode())[0].decode('utf-8'))
+        tokens = self._call_tokenizer_process(program_string, 'ocaml/main.native')
         for pair in tokens:
             yield tuple(pair)
+
+
+class PyretTokenizer(Tokenizer):
+    def tokenize(self, program_string):
+        tokens = self._call_tokenizer_process(program_string, 'pyret/main.sh')
+        for [k, v] in tokens:
+            if k in ['NAME', 'STRING']:
+                yield k
+            else:
+                yield (k, v)
