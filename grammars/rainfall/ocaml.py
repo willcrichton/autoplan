417798bf9d4260f74e6a38859a0466accf8d500d
Georgia <gsamp@stanford.edu>
2019-08-22 16:06:59 -0700

    Revert "Reset file deletion, resolve ocaml.py conflict"
    This reverts commit 8be49a0527e5bde1ee50fb5d9ec314387df688a8.

--- grammars/rainfall/ocaml.py
+++ grammars/rainfall/ocaml.py
@@ -163,7 +163,6 @@ let {{recursion}} counter_helper_name {{params}}
     {%- endif -%}
 {%- endset -%}
 
-
 {% if not anonymous_helpers and not helper_in_body -%}
 {{addition_helper}}
 {{counter_helper}} \n
@@ -178,8 +177,9 @@ let {{recursion}} rainfall {{params}}
     (fun var var -> (if (var = (-999)) then {{failure}} else if (var < 0) then var else (var + var))) list_name 0) /{{dot}} (List.fold_right
         (fun var var -> (if (var = (-999)) then {{failure}} else if (var < 0) then var else (1 + var))) list_name 0)) with division_by_zero_helper_name
         -> {{failure}})
-    
-    {% elif not anonymous_helpers and not helper_in_body -%}
+    {% endif -%}
+
+    {% if not anonymous_helpers and not helper_in_body -%}
     {{rainfall_body}}
     {% endif -%}
 '''
